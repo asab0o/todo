48361daf70ee43a8f1fd51bc6cde3cfc551539ea
0512br <asa.pf1205@gmail.com>
2021-10-25 22:05:13 +0900

    create navber 1

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{csrf_token() }}">
+  <title>@yield('title')</title>
+  <script src=""></script>
+  <link href=""></link>
+  <link href=""></link>
+</head>
+<body>
+  <div class="">
+    <!-- ナビゲーションバー -->
+    <nav class="navber navbar-expand-md navber-dark">
+      <!-- ロゴ -->
+      <div class="cotainer" id="navber">
+        <a class="navber-brand" href="">
+          <img src="">
+        </a>
+      </div>
+    </nav>
+  </div>
+
+
+
+
+</body>
+</html>
